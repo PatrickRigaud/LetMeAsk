ca0973671df5d4b1c05bd8324a9c49a4b6ea40e3
Patrick Rigaud <patrick_rigaud@hotmail.com>
2023-06-20 12:32:42 -0300

    feat(perguntasCaixas) - Criação de hover para os icones, foi alterado como chamar as urls, feito no css

--- src/components/PerguntasCaixas.js
+++ src/components/PerguntasCaixas.js
@@ -1,8 +1,5 @@
 import React, { useState } from 'react';
 import icone_usuario from '../assets/icone_usuario.svg'
-import check from '../assets/checkmark-circle-1.svg'
-import excluir from '../assets/Excluir.svg'
-import messages from '../assets/Messages.svg'
 import '../styles/styleSala.css'
 import {ExcluirPergunta} from '../components/ModalExcluirPergunta';
 
@@ -10,7 +7,8 @@ import {ExcluirPergunta} from '../components/ModalExcluirPergunta';
 export function Perguntas({textoPergunta, nomeUsuario, id, setLista, lista, atualizarQuantidadePerguntas}){
 
     const [openModal, setModal] = useState(false);
-        
+
+
         return <div className="pergunta"  key={id}>
             <p className='textoPergunta'>{textoPergunta}</p>
             <footer>
@@ -20,10 +18,10 @@ export function Perguntas({textoPergunta, nomeUsuario, id, setLista, lista, atua
                 </div>
     
                 <div className="icones">
-                    <img src={check} alt="check"></img>
-                    <img src={excluir} alt="excluir" onClick={() =>
+                    <div className="check"></div>
+                    <div className="excluir" onClick={() =>
                         setModal(true)}/>
-                    <img src={messages} alt="messages"></img>
+                    <div className="messages"></div>
                 </div>
             </footer>
             <ExcluirPergunta 
