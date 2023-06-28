90b595ecf2f620c83f016c2925373c6d819a0e44
Patrick Rigaud <patrick_rigaud@hotmail.com>
2023-06-27 22:17:26 -0300

    feat(telaLogin) - Criação de estrutura para acessar tela pelo pesquisar

--- src/components/telaLogin.js
+++ src/components/telaLogin.js
@@ -3,10 +3,17 @@ import login from '../assets/login.svg'
 import '../styles/styleTelaLogin.css'
 import logo from '../assets/logo_full.svg'
 import { Link } from 'react-router-dom'
+import { useRef } from 'react'
+import { listaSalas } from '../bancoGeral/bancoSalas'
 
 
 
 export function TelaLogin(){
+
+    let inputCodigoSala = useRef(null)
+
+ 
+    
 
     return (<>
 
@@ -25,12 +32,22 @@ export function TelaLogin(){
                 </Link>
                 <form className='opcoes'>
                     <h2 className="textoLogin">-- Entre em uma sala --</h2>
-                    <input placeholder='Digite o código da sala' className='inp_codigo_sala'/>
+                    <input placeholder='Digite o código da sala' ref={inputCodigoSala} className='inp_codigo_sala'/>
                     
-                    <Link to="/sala">
-                        <button className="btn_entrar"> <img src={login} alt="imagem descrição" className='iconLogin'/>Entrar na sala</button>
+                    <Link to={`/sala`}>
+                        <button type="button" className="btn_entrar" onClick={(e)=> {
+                            const achou = listaSalas.find(elemento => elemento.idSala == inputCodigoSala.current.value)
+                            if(achou){
+                                localStorage.setItem('id', `${achou.idSala}`)
+                                localStorage.setItem('nomeSala', `${achou.nome}`)
+                            }else{
+                                e.preventDefault()
+                                alert('Não há sala com esse código')
+                            }
+                            }}> <img src={login} alt="imagem descrição" className='iconLogin' />Entrar na sala</button>
+                    
                     </Link>
-                    
+
                 </form>
             </section>
         </div>
