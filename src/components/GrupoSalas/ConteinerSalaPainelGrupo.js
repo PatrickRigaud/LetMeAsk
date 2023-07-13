b796176b780d2e8f31ada4725f0eeffbe4d776a7
Patrick Rigaud <patrick_rigaud@hotmail.com>
2023-07-13 08:24:28 -0300

    feat(conteinerSalaPainelGrupo) inicio da construção da requisição para o backend da listagem correta da sala

--- src/components/GrupoSalas/ConteinerSalaPainelGrupo.js
+++ src/components/GrupoSalas/ConteinerSalaPainelGrupo.js
@@ -8,6 +8,24 @@ export function ConteinerSalaPainelGrupo({nomeSala, id}){
 
     const setNome = useContext(nomeSalaContext);
     const setId = useContext(iDSalaContext);
+
+    fetch('http://localhost:4000/askFilterID', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({ data: id }), // Converte o objeto para uma string JSON
+})
+  .then(response => response.json()) // Converte a resposta para JSON
+  .then(data => {
+    // Aqui você pode lidar com a resposta do back-end
+    console.log(data);
+  })
+  .catch(error => {
+    // Lida com erros caso ocorra algum problema na requisição
+    console.error('Erro:', error);
+  });
+
  
     return (<>
         <Link to={`/sala`} className="link" onClick={()=>{
