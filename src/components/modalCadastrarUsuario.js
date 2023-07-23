79aa31c181d79f554d1f8f1177b97c737f8437b2
Patrick Rigaud <patrick_rigaud@hotmail.com>
2023-07-23 16:06:00 -0300

    feat(modalCadastrarUsuario) adicionado estrutura para cadastrar usuario

--- src/components/modalCadastrarUsuario.js
+++ src/components/modalCadastrarUsuario.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import React, { useRef, useState } from 'react';
 
 
 const FormOp = styled.form`
@@ -15,7 +16,7 @@ margin: 0;
 
 const CaixaDialogo = styled.main`
     padding-top: 10px;
-    height: 400px;
+    height: 420px;
 `
 const Botoes = styled.div`
 display: flex;
@@ -29,25 +30,71 @@ const BtnEntrar = styled.button`
 margin-top: 0;
 `
 
+const MensagemErro = styled.span`
+color: red;
+`
+
 export function ModalCadastrarUsuario({isOpen, setModalOpen}){
 
-    
+    let nome = useRef(null)
+    let sobrenome = useRef(null)
+    let email = useRef(null)
+    let senha = useRef(null)
+    const [mensagemErro, setMensagemErro] = useState('')
+
+
+    async function fetchEnviarCadastro(){ // Requisição para enviar dados do usuario para o banco
+        await fetch('http://localhost:4000/usuario', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ 
+            nome: nome.current.value,
+            sobrenome: sobrenome.current.value, 
+            email: email.current.value,
+            senha: senha.current.value}),
+        }).then(response => response.json()) 
+        .then(data => {
+            if(data.message){
+                setMensagemErro(data.message)
+            }
+            if(data.fechar){
+                setModalOpen()
+                setMensagemErro('')
+            }
+          
+        })
+          .catch(error => {
+            console.error('Erro:', error);
+          });
+      }
 
     if(isOpen){
     return (<div className="telaInteira">
     <CaixaDialogo className="caixaDialogo">
             <Cabeçalho>Cadastre-se</Cabeçalho>
         <FormOp className="botoes">
-        <input placeholder='Nome' className='inp_codigo_sala'/>
-        <input placeholder='Sobrenome' className='inp_codigo_sala'/>
-        <input placeholder='Email' className='inp_codigo_sala'/>
-        <input placeholder='Senha' type="password" className='inp_codigo_sala'/>
+        <input placeholder='Nome' ref={nome} required className='inp_codigo_sala'/>
+        <input placeholder='Sobrenome' required  ref={sobrenome} className='inp_codigo_sala'/>
+        <input placeholder='Email' required ref={email} className='inp_codigo_sala'/>
+        <input placeholder='Senha' required ref={senha} type="password" className='inp_codigo_sala'/>
 
 
             <Botoes>
-                <BtnEntrar className="btn_entrar">Cadastrar</BtnEntrar>
-                <button className="btnCancelar" onClick={setModalOpen}>Cancelar</button>
+                <BtnEntrar type="button" className="btn_entrar" onClick={() => {
+                    if( nome.current.value.length > 2 && 
+                        sobrenome.current.value.length > 2 && 
+                        email.current.value.length > 2 && 
+                        senha.current.value.length > 2 ){
+                            fetchEnviarCadastro()
+                        }
+                   }}>Cadastrar</BtnEntrar>
+                <button className="btnCancelar" onClick={() => {
+                    setModalOpen()
+                    setMensagemErro('')}}>Cancelar</button>
             </Botoes>
+            <MensagemErro>{mensagemErro}</MensagemErro>
         </FormOp>
     </CaixaDialogo>
 </div>)}
